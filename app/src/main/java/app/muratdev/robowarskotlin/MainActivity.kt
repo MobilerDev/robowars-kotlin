5b6ae56af09a5c558333673f7c17c285c086309c
Murat Terzioglu <muraterzioglu@outlook.com.tr>
2021-03-14 15:33:17 +0300

    Seekbar Live Data Show

--- app/src/main/java/app/muratdev/robowarskotlin/MainActivity.kt
+++ app/src/main/java/app/muratdev/robowarskotlin/MainActivity.kt
@@ -1,46 +1,50 @@
 package app.muratdev.robowarskotlin
 
-import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
 
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_to_firestore, R.id.nav_to_realtimedb), drawerLayout)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-    }
+        val seekLeft = findViewById<SeekBar>(R.id.seekBarLeft)
+        seekLeft?.setOnSeekBarChangeListener(object :
+                SeekBar.OnSeekBarChangeListener {
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
+                    override fun onProgressChanged(seek: SeekBar,
+                        progress: Int, fromUser: Boolean) {
+                            // write custom code for progress is changed
+                            textViewLeft.text = (seek.progress-100).toString();
+                    }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+                    override fun onStartTrackingTouch(seek: SeekBar) { }
+                    override fun onStopTrackingTouch(seek: SeekBar) { }
+
+                }
+        )
+
+        val seekRight = findViewById<SeekBar>(R.id.seekBarRight)
+                seekRight?.setOnSeekBarChangeListener(object :
+                        SeekBar.OnSeekBarChangeListener {
+
+                    override fun onProgressChanged(seek: SeekBar,
+                        progress: Int, fromUser: Boolean) {
+                            // write custom code for progress is changed
+                            textViewRight.text = (seek.progress-100).toString();
+                    }
+
+                    override fun onStartTrackingTouch(seek: SeekBar) { }
+                    override fun onStopTrackingTouch(seek: SeekBar) { }
+
+                }
+        )
     }
 }
